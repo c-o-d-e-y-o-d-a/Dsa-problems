09cc1eb77ac14f5f0d0f5911aa73a69f84bde62d
nischal gautam <nischalgautam7777@gmail.com>
2023-07-02 23:15:21 +0530

    added a different approach

--- LeetCode java/array problems/Q_1512.java
+++ LeetCode java/array problems/Q_1512.java
@@ -17,3 +17,21 @@ class Solution {
 
 
 // code to solve in tc - o(n) and sc - o(n),sacrifices space for time
+// code from someone else
+class Solution {
+    public int numIdenticalPairs(int[] guestList) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        int ans = 0;
+        
+        for(int friend:guestList)
+        {
+            int friendCount = hm.getOrDefault(friend,0);
+            ans+=friendCount;
+            hm.put(friend,friendCount+1);
+        }
+        
+        
+        return ans;
+    }
+}
