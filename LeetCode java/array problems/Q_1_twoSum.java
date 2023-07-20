6f758217a358eb6c768e14bc4ee4d7157fe8a19e
nischal gautam <nischalgautam7777@gmail.com>
2023-07-20 16:04:44 +0530

    BETTTER APPROACH

--- LeetCode java/array problems/Q_1_twoSum.java
+++ LeetCode java/array problems/Q_1_twoSum.java
@@ -1,4 +1,12 @@
-/* brute force solution with tc- O(n^2) and sc - O(1)*/
+/* brute force solution with tc- O(n^2) and sc - O(1)
+Runtime
+49 ms
+Beats
+43.43%
+Memory
+43.6 MB
+Beats
+91.40%*/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] resultArray = new int[2];
@@ -10,6 +18,48 @@ class Solution {
                 }
             }
         }
+        return resultArray;
+
+        
+    }
+}
+/* bettter solution with tc-O(n) and sc -O(n), uses hashmap (NOTE - IN HASHMAPS YOU DON'T NEED TO ITERATE
+OVER EACH ELEMENT TO CHECK IF THEY CONTAIN A CERTAIN ELEMENT) to store the difference between the target and 
+each integer in array and then checks if the difference is equal to any integer in the array , overall better 
+Runtime
+5 ms
+Beats
+59.24%
+Memory
+43.4 MB
+Beats
+96.17% */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] resultArray = new int[2];
+         HashMap<Integer, Integer> difference = new HashMap<Integer, Integer>();
+
+        for(int i=0;i<nums.length;i++){
+            difference.put(target-nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(difference.containsKey(nums[i])){
+                if(i!=difference.get(nums[i])){
+                    resultArray[0]=i;
+                    resultArray[1]=difference.get(nums[i]);
+
+                }
+                
+            }
+        }
+        
+
+
+
+            
+            
+        
         return resultArray;
 
         
