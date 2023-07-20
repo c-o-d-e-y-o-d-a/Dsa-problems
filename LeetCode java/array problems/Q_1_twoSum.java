c27753a7dfd481729bcd4957deb9136d51d875b8
nischal gautam <nischalgautam7777@gmail.com>
2023-07-20 17:01:31 +0530

    all methods

--- LeetCode java/array problems/Q_1_twoSum.java
+++ LeetCode java/array problems/Q_1_twoSum.java
@@ -65,3 +65,71 @@ class Solution {
         
     }
 }
+
+/* Best solution using two pointers currently only working for 55/57 test cases*/
+/* pointer method explanation -
+- you need a sorted array for this method to work
+- then you need to have one pointer which starts from first index and another that starts from last
+- on adding the integer at position of first pointer with the element at the index of second pointer
+if the sum is greater than the , just see the code its self explanatory */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] resultArray = new int[2];
+        int[] nums2 = nums.clone();
+        int start = 0;
+        int endIndex = nums.length-1;
+        Arrays.sort(nums);
+        
+
+        while(start!=endIndex){
+            if(nums[start]+nums[endIndex]>target){
+                endIndex--;
+            }
+            if(nums[start]+nums[endIndex]<target){
+                start++;
+            }
+            if(nums[start]+nums[endIndex]==target){
+                break;
+            }
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(nums2[i]==nums[start]){
+                resultArray[0]=i;
+
+            }
+            else if(nums2[i]==nums[endIndex]){
+                resultArray[1]=i;
+            }
+        }
+         
+        
+        
+
+
+
+            
+            
+        
+        return resultArray;
+
+        
+    }
+}
+
+
+/* hashmap method by using a single for loop(from leetcode editorial) */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
