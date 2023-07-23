d58a8efa2201a3600b312647156819e13ebc0cac
nischal gautam <nischalgautam7777@gmail.com>
2023-07-23 22:25:15 +0530

    beets 100% code

--- LeetCode java/string problems/2011. FinalValueOfVariable_After_Performing_Operations.java
+++ LeetCode java/string problems/2011. FinalValueOfVariable_After_Performing_Operations.java
@@ -26,3 +26,30 @@ class Solution {
         
     }
 }
+/* small change in code to beat 100% in rum time , but you will sacrifice space
+
+Runtime
+Details
+-ms
+Beats 100.00%of users with Java
+Memory
+Details
+41.70mb
+Beats 48.12%of users with Java */
+
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int finalAns = 0;
+        for(int i=0;i<operations.length;i++){
+            
+            if(operations[i].charAt(1)=='+'){
+                finalAns++;
+            }
+            else{
+                finalAns--;
+            }
+        }
+        return finalAns;
+        
+    }
+}
