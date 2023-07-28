77b4761b90eea62ffdae2c71b19caf3b3a9c957a
nischal gautam <nischalgautam7777@gmail.com>
2023-07-28 23:11:52 +0530

    full approach for the question

--- LeetCode java/string problems/nextSmallestPalindrome.java
+++ LeetCode java/string problems/nextSmallestPalindrome.java
@@ -12,4 +12,23 @@ for the 1st case we just add 1 to the number to get the next smallest palindrome
 
 
 for the 2nd case, its a bit more tricky, 
+there are 2 possible scnearios now , either the string has odd length or even length
+for odd length - 
+
+we divide the string from the middle element and then reverse the left side and compare that reversed version with the normal right side
+now there will be 2 possible scenarios either the left side is bigger or its not 
+if its bigger after reversing than we just return left side + middle element + reverse of left side
+if its smaller than we take the left side along with the middle element as a int and and add  to that integer
+after adding 1 we will again take the left hand side of the middle value (which has changed now) and reverse it and return
+left side + middle element + reverse of left side
+
+
+
+for even length- 
+you take the left side and compare it with the right side (both are of equal length) if the reverseof left side is greater than 
+the right side then you return leftside + reverseOfLeftSide 
+
+if its not then you add 1 to the int version of left side and return 
+leftside + reverseOfLeftSide 
+
  */
