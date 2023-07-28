8f6b0694c6f9e903d8f55f86f384a79469007469
nischal gautam <nischalgautam7777@gmail.com>
2023-07-28 23:04:45 +0530

    theory of problem

---
+++ LeetCode java/string problems/nextSmallestPalindrome.java
@@ -0,0 +1,15 @@
+/* 
+the brute way to solve this problem includes increasing the number by 1 and then checking if its a palindrome and
+returning it if it is, but this is the not he best solution 
+
+the better solution is what makes it hard , the trick is quite simple , 
+
+There are 2 possible scenarios - 
+1-  the string is all 9s
+2- the string some other number
+
+for the 1st case we just add 1 to the number to get the next smallest palindrome - ex - 99+1 = 101 and so on
+
+
+for the 2nd case, its a bit more tricky, 
+ */
