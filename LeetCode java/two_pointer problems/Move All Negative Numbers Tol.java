35f618d39c8d0cb9def22b74a272d57cc8b6bae4
nischal gautam <nischalgautam7777@gmail.com>
2023-08-16 16:23:09 +0530

    added better approach

--- LeetCode java/two_pointer problems/Move All Negative Numbers Tol.java
+++ LeetCode java/two_pointer problems/Move All Negative Numbers Tol.java
@@ -48,3 +48,68 @@ public class Solution {
     }
     
 } //another solution
+
+
+// code with right approach but exceeds time limit 
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] separateNegativeAndPositive(int a[]) {
+        int left = 0;
+        int right = a.length-1;
+        while(right>left){
+            if(a[left]<0 && a[right]>0){
+                left++;
+                
+
+            }
+            else if(a[left]>0 && a[right]>0){
+                right--;
+            }
+            else if(a[left]>0 && a[right]<0){
+                int temp = a[left];
+                a[left] = a[right];
+                a[right] = temp;
+                left++;
+                right--;
+            }
+            else if(a[left]<0 && a[right]<0){
+                left++;
+
+            }
+        }
+        return a;
+    }
+    
+}
+
+
+//most optimised approach
+
+/*
+
+you basically have two pointers , one which starts from the beginning and goes over each element to check if they are negative 
+and anothert to swap the elements in the beginning , swapping is done only when a negative no is found by the pointer going over each element , even 
+if it finds a neg element and we are on a neg no it still swaps them
+ */
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] separateNegativeAndPositive(int nums[]) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+
+            if(nums[i]<0){
+                if(i != j){ 
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+                ++j;
+            }
+        }
+        return nums;
+    }
+    
+}
