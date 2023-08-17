13dc3513bef4e0855388daf0e65b612851ce63a9
nischal gautam <nischalgautam7777@gmail.com>
2023-08-17 20:43:18 +0530

    added a solution to the spiral problem ,solved the looked at the solution on youtuge

--- LeetCode java/multi _dimensional_array/print_spiral.java
+++ LeetCode java/multi _dimensional_array/print_spiral.java
@@ -1,6 +1,45 @@
-/* hard question lol  
-what i have figured till now -
+/* What to do to solve the question (aprroach)-
+
+have pointers to store the top index , bottom index , rightmost index and leftmost index for each iteration, you update these 4 pointer values after each
+loop to update
 
 
 
 */
+
+
+
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {  
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {  
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return res;
+    }
+}
