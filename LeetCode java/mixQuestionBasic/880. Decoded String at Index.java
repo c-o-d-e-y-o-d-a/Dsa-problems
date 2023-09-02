bb0f924d53026f517ecb3718f89a0abc83cf6f95
nischal gautam <nischalgautam7777@gmail.com>
2023-09-03 01:08:44 +0530

    couldn't do on my own

---
+++ LeetCode java/mixQuestionBasic/880. Decoded String at Index.java
@@ -0,0 +1,58 @@
+/*
+You are given an encoded string s. To decode the string to a tape, the encoded string is read one character at a time and the following steps are taken:
+
+If the character read is a letter, that letter is written onto the tape.
+If the character read is a digit d, the entire current tape is repeatedly written d - 1 more times in total.
+Given an integer k, return the kth letter (1-indexed) in the decoded string.
+
+ 
+
+Example 1:
+
+Input: s = "leet2code3", k = 10
+Output: "o"
+Explanation: The decoded string is "leetleetcodeleetleetcodeleetleetcode".
+The 10th letter in the string is "o".
+Example 2:
+
+Input: s = "ha22", k = 5
+Output: "h"
+Explanation: The decoded string is "hahahaha".
+The 5th letter is "h".
+Example 3:
+
+Input: s = "a2345678999999999999999", k = 1
+Output: "a"
+Explanation: The decoded string is "a" repeated 8301530446056247680 times.
+The 1st letter is "a".
+ */
+
+sol-
+class Solution {
+    public String decodeAtIndex(String s, int k) {
+        long size = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                size = size * Integer.parseInt(""+ch);
+            }
+            else{size++;}
+        }
+        
+
+        for(int i=s.length()-1;i>=0;i--){
+            char ch = s.charAt(i);
+            k %= size;
+           
+            if(k==0 && Character.isDigit(ch)==false){
+                return ""+ch;
+            }
+             
+            if(Character.isDigit(ch)){
+                size = size/(Integer.parseInt(""+ch));
+            }
+            else{size--;}
+        }
+        return "";
+    }
+}
