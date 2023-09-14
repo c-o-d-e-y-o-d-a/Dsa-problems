f2bba3772c237e3885115add0e7e3dbd7a44c403
nischal gautam <nischalgautam7777@gmail.com>
2023-09-14 22:49:01 +0530

    better solution to a problem based on kandane's algo

--- LeetCode java/mixQuestionBasic/Maximum subarray sum after K concatenation.java
+++ LeetCode java/mixQuestionBasic/Maximum subarray sum after K concatenation.java
@@ -48,3 +48,64 @@ public class Solution {
 	}
 }
 
+// better approach - if k i.e. the no of times you need to concat is 1 , then simply return the kandane's sum of that array
+//if k is greater than 1 and sum of all elements in the array is negative , then max sum will be equal to when kandane's algo is applied
+//after concatening the array at the end once , if sum is +ve , then the greatest sum can be found using the formulae - kandaneSumOf2Arrays + (K-2) * sum of the array;
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int kandaneSum(ArrayList<Integer> arr1){
+		
+		int msf = Integer.MIN_VALUE;
+		int cMaxVal = 0;
+		for(int i=0;i<arr1.size();i++){
+			cMaxVal = Math.max(arr1.get(i), cMaxVal + arr1.get(i));
+			msf = Math.max(cMaxVal, msf);
+		}
+		return msf;
+	}
+
+	public static int kandaneSumOfTwo(ArrayList<Integer> arr1,int n){
+		for(int i=0;i<n;i++){
+			arr1.add(arr1.get(i));
+		}
+		return kandaneSum(arr1);
+	}
+
+	public static long maxSubSumKConcat(ArrayList<Integer> arr, int n, int k) {
+		int sum =0;
+
+		for(int i=0;i<arr.size();i++){
+			sum += arr.get(i);
+		}
+		if(k==0)return 0;
+		if(k==1) return (long) kandaneSum(arr);
+		else if(sum<0) return (long)kandaneSumOfTwo(arr,n);
+		else{return (long)(kandaneSumOfTwo(arr,n) + (k-2)*sum);}
+		 
+		
+	}
+}
+
+
+//another approach: most basic thing to do is to get back to the starting index of the array using the modulo '%' property
+
+
+import java.util.*;
+
+public class Solution {
+
+    public static long maxSubSumKConcat(ArrayList<Integer> arr, int n, int k) {
+
+        int msf = Integer.MIN_VALUE;
+        int mrn = 0;
+
+        for (int i = 0; i < n * k; i++) {
+            mrn = Math.max(arr.get(i % n), mrn + arr.get(i % n));
+            msf = Math.max(msf, mrn);
+        }
+
+        return (long) msf;
+    }
+}
