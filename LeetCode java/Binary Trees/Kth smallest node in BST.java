c1c94971a87b632aa798f572a516482f75667ee8
nischal gautam <nischalgautam7777@gmail.com>
2023-11-28 21:19:55 +0530

    optimised approach

--- LeetCode java/Binary Trees/Kth smallest node in BST.java
+++ LeetCode java/Binary Trees/Kth smallest node in BST.java
@@ -15,3 +15,33 @@ public class Solution {
         return ans.get(k-1);
     }
 }
+
+//same approach (using inorder to find the required element)
+//but without using the arraylist to store the order
+
+public class Solution {
+    public static void helper(TreeNode root,int[] elementNo,int k,int[] arr){
+        if(root  == null )return;
+        helper(root.left,elementNo,k,arr);
+        elementNo[0]++;
+        if(k==elementNo[0]){
+            arr[0] = root.data;
+            return;
+            }
+
+        helper(root.right,elementNo,k,arr);
+
+
+
+
+    }
+    public static int kthSmallest(TreeNode root, int k) {
+        int[] arr = new int[1];
+        int[] el = new int[1];
+        arr[0] = 0;
+        el[0] = 0;
+        helper(root,el,k,arr);
+        return arr[0];
+        
+    }
+} 
