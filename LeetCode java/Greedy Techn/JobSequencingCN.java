a7fb40739c7121bb15efb1cd47d8eb5cc43d1785
nischal gautam <nischalgautam7777@gmail.com>
2024-01-12 23:44:57 +0530

    optimised code job seq problem cn

--- LeetCode java/Greedy Techn/JobSequencingCN.java
+++ LeetCode java/Greedy Techn/JobSequencingCN.java
@@ -139,3 +139,86 @@ public class Solution {
     }
 } //passes 28/80 test cases
 
+//passes all test cases - simplified logic to find a slot for the jobs
+
+import java.util.*;
+
+class Job{
+    int id;
+    int dedline;
+    int money;
+    Job(int id,int dedline,int money){
+        this.id = id;
+        this.dedline = dedline;
+        this.money = money;
+
+    }
+}
+class jobComp implements Comparator<Job> {
+    @Override
+    public int compare(Job a, Job b) {
+        if (a.money > b.money) {
+            return -1;
+        } else if (a.money < b.money) {
+            return 1;
+        } else {
+            if (a.dedline < b.dedline) {
+                return -1;
+            } else if (a.dedline > b.dedline) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
+
+public class Solution {
+    public static int[] jobScheduling(int[][] jobs) {
+    int maxDeadline = 0;
+    for (int[] job : jobs) {
+        maxDeadline = Math.max(maxDeadline, job[1]);
+    }
+
+    int[] flagArr = new int[maxDeadline];
+    Arrays.fill(flagArr, -1);
+
+    ArrayList<Job> joblist = new ArrayList<Job>();
+    for (int i = 0; i < jobs.length; i++) {
+        int[] a = jobs[i];
+        Job a2 = new Job(a[0], a[1], a[2]);
+        joblist.add(a2);
+    }
+
+    jobComp jc = new jobComp();
+    Collections.sort(joblist, jc);
+
+    for (int i = 0; i < joblist.size(); i++) {
+        Job a = joblist.get(i);
+        for (int j = a.dedline - 1; j >= 0; j--) {
+            if (flagArr[j] == -1) {
+                flagArr[j] = a.money;
+                break;
+            }
+        }
+    }
+
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < flagArr.length; i++) {
+        if (flagArr[i] != -1) {
+            count++;
+            sum += flagArr[i];
+        }
+    }
+
+    int[] ansArray = new int[2];
+    ansArray[0] = count;
+    ansArray[1] = sum;
+
+    return ansArray;
+}
+
+
+
+    
+}
