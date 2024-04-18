66608995cc790996b9d20d38e589e8f996f085f3
nischal gautam <nischalgautam7777@gmail.com>
2024-04-18 11:17:31 +0530

    doubly linked list easy

---
+++ LeetCode java/LinkedList/Delete Last Node of a Doubly Linked List.java
@@ -0,0 +1,49 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next, Node prev)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = prev;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node deleteLastNode(Node head) {
+        if(head == null)return head;
+        if(head.next == null)return null;
+        Node curr = head;
+        
+        while(curr.next!=null){
+            curr = curr.next;
+            
+        }
+        curr.prev.next = null;
+        return head;
+
+    }
+}
